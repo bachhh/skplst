35c593af12e52e36f98a1a05f5fe886f73023c74
bachhh <bachhh@users.noreply.github.com>
2022-06-08 21:09:49 +0700

    unh

--- list.go
+++ list.go
@@ -3,11 +3,13 @@ package skplst
 import "math/rand"
 
 const MAXLEVEL = 10
+const NOTFOUND = -1
 
 var NIL *Node = nil
 
 // SkipList header of the skiplist
 type SkipList struct {
+	level   uint
 	forward []*Node
 }
 
@@ -22,13 +24,27 @@ type Node struct {
 
 // level 0 is the leaf node, so
 func (this *SkipList) generateLevel() uint {
+	r := rand.Int31()
+	// instead of calling rand n times, call once and count num of consecutive 1s
 	var n uint = 0
-	for rand.Int()%2 == 0 && n < MAXLEVEL {
+	for r&(1<<n) > 0 {
 		n++
 	}
 	return n
 }
 
-func (s *SkipList) Search(key int) {
+func (s *SkipList) Search(k int) bool {
+	x := s.forward[0]
+	for i := s.level; i > 0; i-- {
+		for x.forward[i].key < k {
+			x = x.forward[i]
+		}
+	}
 
+	x = x.forward[1]
+	if x.key == k {
+		return true
+	}
+
+	return false
 }
